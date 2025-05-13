165abce29016c0c831cb0149ce2bae62f48927db
dryezl <dryezl@foxmail.com>
2025-05-13 16:30:29 +0800

    fix #92 increase sync interval to 12 hours

--- src/main.ts
+++ src/main.ts
@@ -197,7 +197,7 @@ export default class SRPlugin extends Plugin {
                         await this.sync();
                         // this.store.save();
                     },
-                    30 * 60 * 1000,
+                    12 * 60 * 60 * 1000,
                 ),
             );
         }
